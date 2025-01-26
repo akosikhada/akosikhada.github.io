f757a3579fe3d8eef23f66a72ef4e2bf09c49954
akosikhada <miguel.enrique.dasalla.github@gmail.com>
2025-01-26 16:18:52 +0800

    feat(constants.js): Update image imports and fix skill name duplicates
    This commit addresses two key improvements in the constants.js file:
    
    1. **Image Imports:** Updates image import paths to ensure they reference the correct locations within the '/assets/icons' directory.
    2. **Skill Name Duplicates:** Eliminates duplicate skill names within the 'skills' data structure to maintain consistency and accuracy.
    
    This message highlights the specific features addressed (image imports and skill names) and categorizes them using the 'feat' type (since they introduce new functionalities).

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,52 +1,3 @@
-// IMPORTED LOGO FROM IMAGES
-
-// import qcu from "../images/logo/qcu.png";
-import kopibara from "../images/logo/kopibara.png";
-import unilink from "../images/logo/unilink.png";
-import job_portal from "../images/logo/job_portal.png";
-import portfolio_1 from "../images/logo/portfolio_1.jpg";
-import portfolio_2 from "../images/logo/portfolio_2.png";
-import crypto_cosmos from "../images/logo/crypto_cosmos.png";
-import musika from "../images/logo/musika.png";
-
-// IMPORTED ICONS FROM IMAGES
-
-import html from "../images/icons/html.png";
-import css from "../images/icons/css.png";
-import js from "../images/icons/js.png";
-import bootstrap from "../images/icons/bootstrap.png";
-import tailwind from "../images/icons/tailwind.png";
-import react from "../images/icons/react.png";
-import ts from "../images/icons/ts.png";
-import next from "../images/icons/next.png";
-import node from "../images/icons/node.png";
-import express from "../images/icons/express.png";
-import csharp from "../images/icons/csharp.png";
-import php from "../images/icons/php.png";
-import mysql from "../images/icons/mysql.png";
-import sqlserver from "../images/icons/sqlserver.png";
-import mongodb from "../images/icons/mongodb.png";
-import firebase from "../images/icons/firebase.png";
-import dotnet from "../images/icons/dotnet.png";
-import mui from "../images/icons/mui.png";
-import framer from "../images/icons/framer.png";
-import git from "../images/icons/git.png";
-import netlify from "../images/icons/netlify.png";
-import vercel from "../images/icons/vercel.png";
-import coingecko from "../images/icons/coingecko.png";
-import github from "../images/icons/github.png";
-import npm from "../images/icons/npm.png";
-import vite from "../images/icons/vite.png";
-import vscode from "../images/icons/vscode.png";
-import visualstudio from "../images/icons/visualstudio.png";
-import paymongo from "../images/icons/paymongo.png";
-import styled from "../images/icons/styled.png";
-import three from "../images/icons/three.png";
-import shadcn from "../images/icons/shadcn.png";
-import socket from "../images/icons/socket.png";
-import zustand from "../images/icons/zustand.png";
-import clerk from "../images/icons/clerk.png";
-
 // ABOUT ME
 
 export const Bio = {
@@ -64,54 +15,105 @@ export const Bio = {
 
 export const skills = [
   {
+    title: "Frontend",
     skills: [
       {
         name: "HTML",
-        image: html,
+        image: "/assets/icons/html.png",
       },
       {
         name: "CSS",
-        image: css,
+        image: "/assets/icons/css.png",
       },
       {
         name: "JavaScript",
-        image: js,
+        image: "/assets/icons/js.png",
       },
       {
         name: "Bootstrap",
-        image: bootstrap,
+        image: "/assets/icons/bootstrap.png",
       },
       {
         name: "Tailwind CSS",
-        image: tailwind,
+        image: "/assets/icons/tailwind.png",
       },
       {
-        name: "React",
-        image: react,
+        name: "React Js",
+        image: "/assets/icons/react.png",
       },
       {
         name: "TypeScript",
-        image: ts,
+        image: "/assets/icons/ts.png",
       },
       {
-        name: "Next JS",
-        image: next,
+        name: "Next Js",
+        image: "/assets/icons/next.png",
       },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
       {
         name: "PHP",
-        image: php,
+        image: "/assets/icons/php.png",
+      },
+      {
+        name: "Node Js",
+        image: "/assets/icons/node.png",
+      },
+      {
+        name: "Express Js",
+        image: "/assets/icons/express.png",
       },
       {
         name: "MySQL",
-        image: mysql,
+        image: "/assets/icons/mysql.png",
       },
       {
         name: "MongoDB",
-        image: mongodb,
+        image: "/assets/icons/mongodb.png",
       },
       {
         name: "Firebase",
-        image: firebase,
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      {
+        name: "Git",
+        image: "/assets/icons/git.png",
+      },
+      {
+        name: "Github",
+        image: "/assets/icons/github.png",
+      },
+      {
+        name: "NPM",
+        image: "/assets/icons/npm.png",
+      },
+      {
+        name: "Vite",
+        image: "/assets/icons/vite.png",
+      },
+      {
+        name: "Visual Studio Code",
+        image: "/assets/icons/vscode.png",
+      },
+      {
+        name: "Visual Studio",
+        image: "/assets/icons/visualstudio.png",
+      },
+      {
+        name: "Vercel",
+        image: "/assets/icons/vercel.png",
+      },
+      {
+        name: "Netlify",
+        image: "/assets/icons/netlify.png",
       },
     ],
   },
@@ -140,23 +142,23 @@ export const projects = [
     date: "January 2025 - Present",
     description:
       "Musika is a sophisticated music player web application developed with a robust tech stack, including React, TypeScript, Tailwind CSS, Shadcn, and Zustand for the frontend, and Node.js, Express.js, MongoDB, Socket.IO, and JavaScript for the backend. Key features include seamless music playback with next/previous song controls, volume adjustment, and an intuitive admin dashboard for managing albums and songs. Leveraging Socket.IO, Musika enables real-time features such as online/offline status, user activity tracking, and a live chat integrated within the application. Utilizing Clerk for secure authentication, Musika ensures a seamless and secure user experience while providing a rich and engaging music listening experience.",
-    image: musika,
+    image: "/assets/images/musika.png",
     tags: [
-      { name: "HTML", icon: html },
-      { name: "CSS", icon: css },
-      { name: "JavaScript", icon: js },
-      { name: "React JS", icon: react },
-      { name: "TypeScript", icon: ts },
-      { name: "Zustand", icon: zustand },
-      { name: "Shadcn UI", icon: shadcn },
-      { name: "Node JS", icon: node },
-      { name: "Express JS", icon: express },
-      { name: "MongoDB", icon: mongodb },
-      { name: "Socket IO", icon: socket },
-      { name: "Clerk", icon: clerk },
-      { name: "Visual Studio Code", icon: vscode },
-      { name: "NPM", icon: npm },
-      { name: "Vite", icon: vite },
+      { name: "HTML", icon: "/assets/icons/html.png" },
+      { name: "CSS", icon: "/assets/icons/css.png" },
+      { name: "JavaScript", icon: "/assets/icons/js.png" },
+      { name: "React JS", icon: "/assets/icons/react.png" },
+      { name: "TypeScript", icon: "/assets/icons/ts.png" },
+      { name: "Zustand", icon: "/assets/icons/zustand.png" },
+      { name: "Shadcn UI", icon: "/assets/icons/shadcn.png" },
+      { name: "Node JS", icon: "/assets/icons/node.png" },
+      { name: "Express JS", icon: "/assets/icons/express.png" },
+      { name: "MongoDB", icon: "/assets/icons/mongodb.png" },
+      { name: "Socket IO", icon: "/assets/icons/socket.png" },
+      { name: "Clerk", icon: "/assets/icons/clerk.png" },
+      { name: "Visual Studio Code", icon: "/assets/icons/vscode.png" },
+      { name: "NPM", icon: "/assets/icons/npm.png" },
+      { name: "Vite", icon: "/assets/icons/vite.png" },
     ],
     source: "",
     demo: "",
@@ -168,19 +170,19 @@ export const projects = [
     date: "January 2025",
     description:
       "Crypto Cosmos is a dynamic and user-friendly web application developed using HTML, CSS, JavaScript, and React.js, designed to provide real-time insights into the cryptocurrency market. Leveraging the comprehensive data provided by the CoinGecko API, the application offers users access to a wide range of cryptocurrencies, including real-time pricing information, market trends, and historical data. With its intuitive interface and robust data integration, Crypto Cosmos empowers users to make informed decisions and stay abreast of the ever-evolving cryptocurrency landscape. This application serves as a valuable resource for both novice and experienced cryptocurrency enthusiasts, providing a user-friendly platform to explore and understand the dynamic world of cryptocurrencies.",
-    image: crypto_cosmos,
+    image: "/assets/images/crypto_cosmos.png",
     tags: [
-      { name: "HTML", icon: html },
-      { name: "CSS", icon: css },
-      { name: "JavaScript", icon: js },
-      { name: "React JS", icon: react },
-      { name: "Visual Studio Code", icon: vscode },
-      { name: "Git", icon: git },
-      { name: "Github", icon: github },
-      { name: "NPM", icon: npm },
-      { name: "Vite", icon: vite },
-      { name: "Vercel", icon: vercel },
-      { name: "CoinGecko", icon: coingecko },
+      { name: "HTML", icon: "/assets/icons/html.png" },
+      { name: "CSS", icon: "/assets/icons/css.png" },
+      { name: "JavaScript", icon: "/assets/icons/js.png" },
+      { name: "React JS", icon: "/assets/icons/react.png" },
+      { name: "Visual Studio Code", icon: "/assets/icons/vscode.png" },
+      { name: "Git", icon: "/assets/icons/git.png" },
+      { name: "Github", icon: "/assets/icons/github.png" },
+      { name: "NPM", icon: "/assets/icons/npm.png" },
+      { name: "Vite", icon: "/assets/icons/vite.png" },
+      { name: "Vercel", icon: "/assets/icons/vercel.png" },
+      { name: "CoinGecko", icon: "/assets/icons/coingecko.png" },
     ],
     source: "",
     demo: "https://cryptocosmos.vercel.app/",
@@ -192,17 +194,17 @@ export const projects = [
     date: "January 2025 - Present",
     description:
       "This portfolio website is a meticulously crafted online showcase built with TypeScript, Next.js, and Tailwind CSS, ensuring high code quality, optimal performance, and a visually stunning user experience. Leveraging the power of Framer Motion, the website incorporates smooth and engaging animations that enhance user interaction and create a dynamic and immersive presentation. Built with a focus on accessibility and user-friendliness, the website utilizes the Simplicity design system to maintain a consistent and elegant aesthetic across all pages. Furthermore, the website is fully responsive, adapting seamlessly to any screen size for an optimal viewing experience on desktops, tablets, and mobile devices.",
-    image: portfolio_2,
+    image: "/assets/images/portfolio_2.png",
     tags: [
-      { name: "TypeScript", icon: ts },
-      { name: "Next JS", icon: next },
-      { name: "Tailwind CSS", icon: tailwind },
-      { name: "Framer Motion", icon: framer },
-      { name: "Git", icon: git },
-      { name: "Github", icon: github },
-      { name: "Visual Studio Code", icon: vscode },
-      { name: "Netlify", icon: netlify },
-      { name: "NPM", icon: npm },
+      { name: "TypeScript", icon: "/assets/icons/ts.png" },
+      { name: "Next JS", icon: "/assets/icons/next.png" },
+      { name: "Tailwind CSS", icon: "/assets/icons/tailwind.png" },
+      { name: "Framer Motion", icon: "/assets/icons/framer.png" },
+      { name: "Git", icon: "/assets/icons/git.png" },
+      { name: "Github", icon: "/assets/icons/github.png" },
+      { name: "Visual Studio Code", icon: "/assets/icons/vscode.png" },
+      { name: "Netlify", icon: "/assets/icons/netlify.png" },
+      { name: "NPM", icon: "/assets/icons/npm.png" },
     ],
     source: "",
     demo: "https://miguelenriquedasalla.netlify.app/",
@@ -214,22 +216,22 @@ export const projects = [
     date: "January 2025 - Present",
     description:
       "This job portal is a full-stack web application developed using the MERN stack, comprising MongoDB, Express.js, React.js, and Node.js. It provides a user-friendly platform for job seekers to browse and apply for available positions, while enabling employers to easily post job openings and manage applications. The application leverages the power of React.js for a dynamic and interactive user interface, enhanced by the sleek and modern styling of Tailwind CSS. By utilizing the MERN stack and a robust database like MongoDB, this job portal offers a scalable and efficient solution for connecting job seekers with employers in the digital age.",
-    image: job_portal,
+    image: "/assets/images/job_portal.png",
     tags: [
-      { name: "HTML", icon: html },
-      { name: "CSS", icon: css },
-      { name: "JavaScript", icon: js },
-      { name: "Tailwind CSS", icon: tailwind },
-      { name: "MongoDB", icon: mongodb },
-      { name: "Express JS", icon: express },
-      { name: "React JS", icon: react },
-      { name: "Node JS", icon: node },
-      { name: "Firebase", icon: firebase },
-      { name: "Git", icon: git },
-      { name: "Github", icon: github },
-      { name: "NPM", icon: npm },
-      { name: "Vite", icon: vite },
-      { name: "Visual Studio Code", icon: vscode },
+      { name: "HTML", icon: "/assets/icons/html.png" },
+      { name: "CSS", icon: "/assets/icons/css.png" },
+      { name: "JavaScript", icon: "/assets/icons/js.png" },
+      { name: "Tailwind CSS", icon: "/assets/icons/tailwind.png" },
+      { name: "MongoDB", icon: "/assets/icons/mongodb.png" },
+      { name: "Express JS", icon: "/assets/icons/express.png" },
+      { name: "React JS", icon: "/assets/icons/react.png" },
+      { name: "Node JS", icon: "/assets/icons/node.png" },
+      { name: "Firebase", icon: "/assets/icons/firebase.png" },
+      { name: "Git", icon: "/assets/icons/git.png" },
+      { name: "Github", icon: "/assets/icons/github.png" },
+      { name: "NPM", icon: "/assets/icons/npm.png" },
+      { name: "Vite", icon: "/assets/icons/vite.png" },
+      { name: "Visual Studio Code", icon: "/assets/icons/vscode.png" },
     ],
     source: "",
     demo: "",
@@ -241,19 +243,19 @@ export const projects = [
     date: "November 2024 - December 2024",
     description:
       "This portfolio website is a visually captivating and immersive online showcase built with React JS, styled-components, Framer Motion, Three.js, and Material UI. Designed with a focus on user experience, the website is fully responsive across all devices, ensuring seamless viewing on desktops, tablets, and smartphones. Utilizing Framer Motion, I've incorporated smooth and engaging animations that bring the portfolio to life, while Three.js adds a touch of interactive 3D elements. Material UI provides a consistent and professional aesthetic, further enhancing the overall user experience.",
-    image: portfolio_1,
+    image: "/assets/images/portfolio_1.png",
     tags: [
-      { name: "HTML", icon: html },
-      { name: "CSS", icon: css },
-      { name: "JavaScript", icon: js },
-      { name: "React JS", icon: react },
-      { name: "Styled Components", icon: styled },
-      { name: "Three JS", icon: three },
-      { name: "Material UI", icon: mui },
-      { name: "Visual Studio Code", icon: vscode },
-      { name: "Git", icon: git },
-      { name: "Github", icon: github },
-      { name: "NPM", icon: npm },
+      { name: "HTML", icon: "/assets/icons/html.png" },
+      { name: "CSS", icon: "/assets/icons/css.png" },
+      { name: "JavaScript", icon: "/assets/icons/js.png" },
+      { name: "React JS", icon: "/assets/icons/react.png" },
+      { name: "Styled Components", icon: "/assets/icons/styled.png" },
+      { name: "Three JS", icon: "/assets/icons/three.png" },
+      { name: "Material UI", icon: "/assets/icons/mui.png" },
+      { name: "Visual Studio Code", icon: "/assets/icons/vscode.png" },
+      { name: "Git", icon: "/assets/icons/git.png" },
+      { name: "Github", icon: "/assets/icons/github.png" },
+      { name: "NPM", icon: "/assets/icons/npm.png" },
     ],
     source: "https://github.com/akosikhada/akosikhada.github.io.git",
     demo: "https://akosikhada.github.io/",
@@ -265,17 +267,17 @@ export const projects = [
     date: "September 2024 - December 2024",
     description:
       "The UNILINK: Academic Community Web Application is an interactive, web-based platform designed specifically for academic institutions Its primary goal is to enhance the management of student information, streamline course scheduling, and facilitate accurate grade viewing. Acting as a comprehensive Student Information System (SIS), UNILINK unites essential academic management tasks into a single, integrated platform. A subsystem of the UNILINK: Academic Community Web Application, designed to streamline student information management and enhance administrative processes.",
-    image: unilink,
+    image: "/assets/images/unilink.png",
     tags: [
-      { name: "HTML", icon: html },
-      { name: "CSS", icon: css },
-      { name: "JavaScript", icon: js },
-      { name: "Bootstrap", icon: bootstrap },
-      { name: "PHP", icon: php },
-      { name: "MySQL", icon: mysql },
-      { name: "Visual Studio Code", icon: vscode },
-      { name: "Git", icon: git },
-      { name: "Github", icon: github },
+      { name: "HTML", icon: "/assets/icons/html.png" },
+      { name: "CSS", icon: "/assets/icons/css.png" },
+      { name: "JavaScript", icon: "/assets/icons/js.png" },
+      { name: "Bootstrap", icon: "/assets/icons/bootstrap.png" },
+      { name: "PHP", icon: "/assets/icons/php.png" },
+      { name: "MySQL", icon: "/assets/icons/mysql.png" },
+      { name: "Visual Studio Code", icon: "/assets/icons/vscode.png" },
+      { name: "Git", icon: "/assets/icons/git.png" },
+      { name: "Github", icon: "/assets/icons/github.png" },
     ],
     source: "",
     demo: "",
@@ -287,19 +289,19 @@ export const projects = [
     date: "September 2024 - December 2024",
     description:
       "KOPIBARA is a modern web-based coffee ordering system developed by the KPL Team as part of the IPT102 - Integrative Programming and Technologies 2 course. Built with ASP.NET Core 8.0, it offers a seamless experience for customers to browse and order a variety of coffee beverages, including both hot and iced options. KOPIBARA empowers customers with a secure and convenient payment gateway powered by Paymongo, ensuring a smooth and hassle-free checkout process. Additionally, the system leverages Entity Framework Core to streamline data access and manipulation, facilitating efficient order management and inventory control.",
-    image: kopibara,
+    image: "/assets/images/kopibara.png",
     tags: [
-      { name: "HTML", icon: html },
-      { name: "CSS", icon: css },
-      { name: "JavaScript", icon: js },
-      { name: "Bootstrap", icon: bootstrap },
-      { name: ".NET", icon: dotnet },
-      { name: "C#", icon: csharp },
-      { name: "SQL Server", icon: sqlserver },
-      { name: "PayMongo", icon: paymongo },
-      { name: "Visual Studio", icon: visualstudio },
-      { name: "Git", icon: git },
-      { name: "Github", icon: github },
+      { name: "HTML", icon: "/assets/icons/html.png" },
+      { name: "CSS", icon: "/assets/icons/css.png" },
+      { name: "JavaScript", icon: "/assets/icons/js.png" },
+      { name: "Bootstrap", icon: "/assets/icons/bootstrap.png" },
+      { name: ".NET", icon: "/assets/icons/dotnet.png" },
+      { name: "C#", icon: "/assets/icons/csharp.png" },
+      { name: "SQL Server", icon: "/assets/icons/sqlserver.png" },
+      { name: "PayMongo", icon: "/assets/icons/paymongo.png" },
+      { name: "Visual Studio", icon: "/assets/icons/visualstudio.png" },
+      { name: "Git", icon: "/assets/icons/git.png" },
+      { name: "Github", icon: "/assets/icons/github.png" },
     ],
     source: "",
     demo: "",
